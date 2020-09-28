c6de00c86fcf7549d89eadf0e0dbfbe1857aab8b
David Li <david.li3100@gmail.com>
2020-09-28 15:11:07 -0400

    feat: full sync of firebase collections

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,49 +1,79 @@
 // Import all needed modules.
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import algoliasearch from 'algoliasearch';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import algoliasearch from "algoliasearch";
+import { error } from "firebase-functions/lib/logger";
 
 // Set up Firestore.
 admin.initializeApp();
 const db = admin.firestore();
 
 // Set up Algolia.
-// The app id and API key are coming from the cloud functions environment, as we set up in Part 1, Step 3.
-const algoliaClient = algoliasearch(functions.config().algolia.appid, functions.config().algolia.apikey);
-// Since I'm using develop and production environments, I'm automatically defining 
-// the index name according to which environment is running. functions.config().projectId is a default 
-// property set by Cloud Functions.
-const collectionIndexName = 'Employers';
-const collectionIndex = algoliaClient.initIndex(collectionIndexName);
+const algoliaClient = algoliasearch(
+  functions.config().algolia.appid,
+  functions.config().algolia.apikey
+);
+
+const syncCollectionToAlgolia = async (
+  collectionName: string,
+  firebaseCollectionName: string
+) => {
+  const collectionIndexName = collectionName;
+  const collectionIndex = algoliaClient.initIndex(collectionIndexName);
+
+  const algoliaRecords: any[] = [];
+
+  // Retrieve all documents from the firebase collection.
+  const querySnapshot = await db.collection(firebaseCollectionName).get();
+
+  querySnapshot.docs.forEach((doc) => {
+    const document = doc.data();
+    let record = {};
+    switch (firebaseCollectionName) {
+      case "employers":
+        record = {
+          objectID: doc.id,
+          imageURL: document.imageURL,
+          name: document.name,
+          numReports: document.numReports,
+          score: document.score,
+          categorized_incidents: document.categorized_incidents,
+				};
+				break;
+      case "reports":
+        record = {
+					objectID: doc.id,
+					date: document.date.toDate(),
+					dateMillis: document.date._seconds * 1000,
+          description: document.description,
+          employer: document.employer,
+          title: document.title,
+          categories: document.categories,
+				};
+				break;
+			default:
+				error('oops, an unhandled collection was called')
+    }
+    algoliaRecords.push(record);
+  });
+
+  // After all records are created, we save them to
+  collectionIndex
+    .saveObjects(algoliaRecords, (_error: any, content: any) => {
+      return true;
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+};
 
 // Create a HTTP request cloud function.
-export const sendEmployersToAlgolia = functions.https.onRequest(async (req, res) => {
+export const sendEmployersToAlgolia = functions.https.onRequest(
+  async (req, res) => {
+		await syncCollectionToAlgolia('Employers', 'employers');
+		await syncCollectionToAlgolia('Reports', 'reports');
 
-	// This array will contain all records to be indexed in Algolia.
-	// A record does not need to necessarily contain all properties of the Firestore document,
-	// only the relevant ones. 
-	const algoliaRecords : any[] = [];
-
-	// Retrieve all documents from the COLLECTION collection.
-	const querySnapshot = await db.collection('employers').get();
-
-	querySnapshot.docs.forEach(doc => {
-		const document = doc.data();
-        const record = {
-            objectID: doc.id,
-            imageURL: document.imageURL,
-						name: document.name,
-						numReports: document.numReports,
-						score: document.score
-        };
-
-        algoliaRecords.push(record);
-    });
-	
-	// After all records are created, we save them to 
-	collectionIndex.saveObjects(algoliaRecords, (_error: any, content: any) => {
-        res.status(200).send("COLLECTION was indexed to Algolia successfully.");
-		})
-		.catch(err => console.error(err));
-	
-})
+		res.status(200).send("Database was indexed to Algolia successfully.");
+  }
+);
