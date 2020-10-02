53e1fe4dabd14a0b3d52c490074c65174c446f76
Aaron Ma <amag1@protonmail.com>
2020-10-02 10:06:55 -0400

    implemented addEmployer

--- functions/src/Firebase/addEmployer.ts
+++ functions/src/Firebase/addEmployer.ts
@@ -1,7 +1,30 @@
+
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
 
 export const addEmployer = functions.https.onCall((data, context) => {
 
 
+    const numEmployees = data.numEmployees;
+    const bgURL = data.backgroundImageURL;
+    const imgURL = data.imageURL;
+    const name = data.name;
+
+    return admin.database().ref("/employers").push({
+        backgroundImageURL:     bgURL,
+        imageURL:               imgURL,
+        categories: {
+            "racism" : 0
+        },
+        categorized_incedents:  0,
+        name:                   name,
+        numEmployees:           numEmployees,
+        numReports: 0,
+        score: 0,
+    }).then(() => {
+        console.log("pushed new employer to database")
+    })
 });
+
